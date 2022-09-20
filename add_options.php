503beb8d4621feea4c4e6d7e78cce854323a88f5
OleksiiOz <110377404+OleksiiOz@users.noreply.github.com>
2022-09-20 23:32:12 +0300

    допил

--- add_options.php
+++ add_options.php
@@ -5,6 +5,8 @@
 	  if (!isset($row)) {
 	  	$sql = "SELECT * FROM options WHERE user_id = 0"; // 0 = default
 	    $result = $conn->query($sql);
-	    $row = $result->fetch_assoc();
+	   	$row = $result->fetch_assoc();
+   		$sql = "INSERT INTO `options` (`user_id`, `letter_count`, `word_count`, `language`) VALUES ('" . $currentUser . "','" .$row['letter_count']. "','".$row['word_count']."','".$row['language']."')";
+	    $result = $conn->query($sql);
 	  }
 ?>
